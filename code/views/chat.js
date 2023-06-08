c71e1b5fb52a0085f2780a55da4f2f8182de0782
Amruth-111 <amruthkrishnaa@gmail.com>
2023-06-08 12:14:54 +0530

    setInterval added

--- code/views/chat.js
+++ code/views/chat.js
@@ -1,16 +1,30 @@
 
 window.addEventListener("DOMContentLoaded",async(e)=>{
-    // e.preventDefault();
-    let token=localStorage.getItem('token');
-    let res=await axios.get("http://localhost:8000/message/all-messages",{headers:{'Authentication':token}})
-    console.log(res.data)
-    for(let i=0;i<res.data.length;i++){
-       
-    }
-    const text=document.createTextNode(res.data.Data[0].message) ;
-    document.getElementById("msg").appendChild(text)
-    
+    e.preventDefault();
+    setInterval(async() => {
+        let token=localStorage.getItem('token');
+        let res=await axios.get("http://localhost:8000/message/all-messages",{headers:{'Authentication':token}})
+        console.log(res.data.Data.length)
+
+        console.log(res.data.Data)
+        for(let i=0;i<res.data.Data.length;i++){
+            showChatOnScreen(res.data.Data[i].message)
+        }
+    }, 1000);
 })
+
+
+
+console.log("sjajkXINK")
+async function showChatOnScreen(msg){
+    try{
+       const parent=document.getElementById("msg")
+       const child=`</li class="text-white">${msg}</li><br>`
+       parent.innerHTML=parent.innerHTML+child
+    }catch(err){
+        console.log("error in showchatonscreen",err)
+    }
+}
 
 let button=document.getElementById("send") 
 let message=document.getElementById("message")
