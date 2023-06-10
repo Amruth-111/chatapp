7850e2abc034d8cd586268998d21564c914e4275
Amruth-111 <amruthkrishnaa@gmail.com>
2023-06-10 16:34:10 +0530

    ready to deploy

--- code/util/databases.js
+++ code/util/databases.js
@@ -1,8 +1,8 @@
 const Sequelize=require('sequelize')
-
-const sequelize=new Sequelize("chat-app",'root',"Admin@123",{
-dialect:'mysql',
-host:'localhost'
+require("dotenv").config();
+const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+    dialect:"mysql",
+    host:process.env.DB_HOST
 })
 
 module.exports=sequelize
